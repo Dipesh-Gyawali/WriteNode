9cf32862914b09e1c800709573dfb6be9e0891d4
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2023-10-05 13:27:22 +0545

    structure

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import { PostCard } from '../components';
 
 export const HomePage = () => {
+  const posts = [
+    { id: 1, title: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet", author: "Dipesh"},
+    { id: 2, title: "Lorem ipsum", description: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet", author: "Gyawali"}
+  ]
+
   return (
-    <div>HomePage</div>
+    <section>
+      { posts.map((post) => ( 
+         <PostCard key={post.id} post={post} />
+
+      ))}
+    </section>
   )
 }
