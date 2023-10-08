b8d0c0c9d07fe6655001627014958a65c3a525dd
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2023-10-08 16:39:34 +0545

    local storage and protected routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,20 @@ import Logo from "../assets/logo.png";
 import { useState } from "react";
 
 export const Header = () => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(JSON.parse(localStorage.getItem("isAuth")) || false);
 
   function handleLogin(){
     signInWithPopup(auth, provider).then((result) => {
       console.log(result);
       setIsAuth(true);
+      localStorage.setItem("isAuth",true);
     })
   }
 
   function handleLogout(){
     signOut(auth);
     setIsAuth(false);
+    localStorage.setItem("isAuth",false);
   }
 
   return (
@@ -33,7 +35,7 @@ export const Header = () => {
             <button onClick={handleLogout} className="auth"><i className="bi bi-box-arrow-right"></i> Logout</button> 
           </>                   
         ) : (
-          <button onClick={handleLogout} className="auth"><i className="bi bi-google"></i> Login</button>
+          <button onClick={handleLogin} className="auth"><i className="bi bi-google"></i> Login</button>
         ) }        
       </nav>
     </header>
